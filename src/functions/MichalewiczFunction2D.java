9d9a26d27ac436c5961446f6a59ed656e07d1152
unknown <budzet123@gmail.com>
2015-01-13 13:30:24 +0100

    dodałem MichalewiczFunction2D

---
+++ src/functions/MichalewiczFunction2D.java
@@ -0,0 +1,8 @@
+package functions;
+
+public class MichalewiczFunction2D {
+	
+	public static double getResult(double x){
+		return 	Math.sin(x) * Math.pow(Math.sin(((i+1) * x * x)/Math.PI), 2*m);
+	}
+}
